0dad12ebd1f70f75548a51a722c8a80f864d4a92
Ignacio2100 <ignaciodeleone8@gmail.com>
2024-03-05 01:30:51 -0600

    creacion de archivo pdf

--- Controllers/fichamedica.controller.js
+++ Controllers/fichamedica.controller.js
@@ -1,7 +1,7 @@
 import {Ficha} from '../Modelos/fichamedica.modelo.js';
+import PDFDocument from 'pdfkit';
+
 const ficha = new Ficha();
-
-
 const getFicha= async(req, res) => {
     try {
         const result = await ficha.getFichas();
@@ -11,9 +11,51 @@ const getFicha= async(req, res) => {
     }
 };
 
-
+const getdescarga = async (req, res) => {
+    try {
+      const { idPaciente } = req.params;
+      const result = await ficha.getFichasPorIdPaciente(idPaciente);
+  
+      if (result && result.length > 0) {
+        const doc = new PDFDocument();
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `inline; filename=Expediente_${idPaciente}.pdf`);
+  
+        // Títulos
+      doc.fontSize(12).text('Expediente Médico', { align: 'center' });
+        // Escribir datos de la base de datos en el PDF
+        result.forEach((fila) => {
+          doc.text(`Codigo Expediente:${fila.Id_Ficha}`);
+          doc.text(`Fecha:${fila.Fecha}`);
+          doc.text(`DPI:${fila.DPI}`);
+          doc.text(`Nombres:${fila.Nombres}`);
+          doc.text(`Apellidos:${fila.Apellidos}`);
+          doc.text(`Motivo Consulta:${fila.Motivo_Consulta}`);
+          doc.text(`DPI:${fila.DPI}`);
+          doc.text(`Codigo Paciente:${fila.Id_Paciente}`);
+          doc.text(`Diagnostico:${fila.Diagnostico}`);
+          doc.text(`Visita:${fila.Proxima_visita}`);
+          doc.text(`Nivel de Azucar:${fila.Nivel_Azucar}`);
+          doc.text(`Tiempo de Tratamiento:${fila.Tiempo_tratamiento}`);
+          doc.text(`Recomendaciones:${fila.Recomendaciones}`);
+          doc.text(`Estado:${fila.Estado}`);
+          doc.text('********************************************************************************************');
+        });
+  
+        // Finalizar el documento y enviar el PDF como stream al cliente
+        doc.end();
+        doc.pipe(res);
+      } else {
+        res.status(404).json({ error: 'Archivo no encontrado para el paciente' });
+      }
+  
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  };
 
 export {
     getFicha,
-    
+    getdescarga
 }
