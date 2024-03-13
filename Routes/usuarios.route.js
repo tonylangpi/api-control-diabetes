40942c5db3c3f4cb65bbc8f971011158924e5ce4
langpineiro <alangp@miumg.edu.gt>
2024-03-13 00:13:10 -0600

    cambios, rutas para crud de usuarios

--- Routes/usuarios.route.js
+++ Routes/usuarios.route.js
@@ -1,10 +1,13 @@
 import {Router} from "express";
-import {getUsers,createUsers} from '../Controllers/usuarios.controller.js';
+import {getUsers,createUsers,inactivateUser,updateUser,UsuarioByID} from '../Controllers/usuarios.controller.js';
 
 const router = Router();
 
 //rutas de CRUD sobre tabla USUARIOS EN BD EN LA NUBE
 router.get('/all',getUsers);
 router.post('/create',createUsers);
+router.get('/usuarioById/:ID_Usuario',UsuarioByID);
+router.put('/inactivateUser/:Id_Usuario',inactivateUser)
+router.put('/updateUser/:Id_Usuario',updateUser)
 
 export default router;
